e4579e24e5e5afa705f2f722972d918385c95530
Ephy Kimotho <ephyk48@gmail.com>
2025-03-04 07:20:46 +0300

    chore: added InputProps and Signup intial values interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -11,3 +11,22 @@ export interface CompanyInfoProps {
     title: string,
     text: string
 }
+
+export interface InputProps {
+    Icon: ComponentType<{ size: number, color: string, strokewidth?: number, onClick?: () => void, className?: string }>,
+    name: string,
+    type: string,
+    placeholder: string,
+    togglePassword?: () => void
+}
+
+
+export interface SignUpInitialValues {
+    firstName: string,
+    lastName: string,
+    email: string,
+    phone: string,
+    password: string,
+}
+
+export type AuthButtonProps = Pick<ButtonProps, "children" | "moreStyles">
