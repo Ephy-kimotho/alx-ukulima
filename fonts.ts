be7c8814994fe4ab167a1a9886c18266d3141356
Ephy Kimotho <ephyk48@gmail.com>
2025-03-05 23:23:51 +0300

    chore: added poppins font

--- fonts.ts
+++ fonts.ts
@@ -1,5 +1,5 @@
 /* Setting up fonts to be used in the application */
-import { Mulish, Nunito, Quicksand, Montserrat } from "next/font/google";
+import { Mulish, Nunito, Quicksand, Montserrat, Poppins } from "next/font/google";
 
 export const mulish = Mulish({
     weight: ["400", "700"],
@@ -18,5 +18,10 @@ export const quicksand = Quicksand({
 
 export const montserrat = Montserrat({
     weight: ["400", "600", "800"],
+    subsets: ["latin"]
+})
+
+export const poppins = Poppins({
+    weight: ["400", "500"],
     subsets: ["latin"]
 })
