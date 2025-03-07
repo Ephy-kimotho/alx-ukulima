df88b644b488d6e58a47cc45f87db4dfc04a3e96
Ephy Kimotho <ephyk48@gmail.com>
2025-03-07 17:01:12 +0300

    chore: added Head metadata to login and sign up

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import Input from "@/components/common/Input";
 import AuthButton from "@/components/common/AuthButton";
 import logo from "@/public/icons/logo.png";
+import Head from "next/head";
 
 function Login() {
   // State to toggle password visibility
@@ -35,102 +36,111 @@ function Login() {
   };
 
   return (
-    <section className={`${nunito.className} flex`}>
-      <div className="hidden md:flex bg-[url(@/public/images/farmImage.png)] bg-left-bottom bg-cover justify-center inset-shadow-translucent flex-1">
-        <article className="space-y-12 text-white mt-28 text-center">
-          <h2 className="font-bold text-4xl">Ukulima</h2>
+    <>
+      <Head>
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
+      <section className={`${nunito.className} flex`}>
+        <div className="hidden md:flex bg-[url(@/public/images/farmImage.png)] bg-left-bottom bg-cover justify-center inset-shadow-translucent flex-1">
+          <article className="space-y-12 text-white mt-28 text-center">
+            <h2 className="font-bold text-4xl">Ukulima</h2>
 
-          <div className="italic font-normal space-y-4 text-lg max-w-[350px]">
-            <p>Empowering Farmers, Connecting Suppliers.</p>
-            <p>
-              -Your trusted marketplace for agricultural products and tools.
-            </p>
-          </div>
-        </article>
-      </div>
-
-      <div className="flex-1">
-        <div className="mt-3 ml-3">
-          <Link href="/">
-            <Image
-              src={logo}
-              alt="ukulima logo"
-              className="w-9 h-9 sm:w-12 sm:h-12"
-            />
-          </Link>
-        </div>
-
-        <Formik
-          initialValues={initialValues}
-          validationSchema={loginSchema}
-          onSubmit={handleSubmit}
-        >
-          <Form className="w-11/12 max-w-2xl mx-auto min-h-[90vh]  relative">
-            <div className="text-center mb-5">
-              <h3 className="text-lime-green font-bold  text-3xl">
-                Welcome back
-              </h3>
-              <p className="text-[#8E8E90]  text-base">
-                Log in to your account
+            <div className="italic font-normal space-y-4 text-lg max-w-[350px]">
+              <p>Empowering Farmers, Connecting Suppliers.</p>
+              <p>
+                -Your trusted marketplace for agricultural products and tools.
               </p>
             </div>
+          </article>
+        </div>
 
-            <div className="space-y-2 mb-6">
-              <label
-                htmlFor="email"
-                className="text-lime-green  font-bold text-lg"
-              >
-                Email
-              </label>
-              <Input
-                type="email"
-                name="email"
-                placeholder="Enter your email."
-                Icon={Mail}
+        <div className="flex-1">
+          <div className="mt-3 ml-3">
+            <Link href="/">
+              <Image
+                src={logo}
+                alt="ukulima logo"
+                className="w-9 h-9 sm:w-12 sm:h-12"
               />
-            </div>
+            </Link>
+          </div>
 
-            <div className="space-y-2">
-              <label
-                htmlFor="password"
-                className="text-lime-green  font-bold text-lg"
-              >
-                Password
-              </label>
-              <Input
-                type={showPassword ? "text" : "password"}
-                name="password"
-                Icon={showPassword ? Eye : EyeOff}
-                placeholder="Enter your password."
-                togglePassword={togglePassword}
-              />
-            </div>
-
-            <div className="absolute bottom-0 left-0 right-0 space-y-4">
-              <div className="text-center">
-                <AuthButton moreStyles="w-2/3 max-w-80 py-3">Log in</AuthButton>
-              </div>
-
-              <div className="text-center pb-5 text-sm space-y-2">
-                <p className="text-gray-400">
-                  Don&apos;t have an account?
-                  <Link
-                    href="/signup"
-                    className="text-lime-green hover:underline"
-                  >
-                    &nbsp;Sign up
-                  </Link>
-                </p>
-                <p className="text-lime-green">
-                  &copy;copyright {new Date().getFullYear()}, All rights
-                  reserved.
+          <Formik
+            initialValues={initialValues}
+            validationSchema={loginSchema}
+            onSubmit={handleSubmit}
+          >
+            <Form className="w-11/12 max-w-2xl mx-auto min-h-[90vh]  relative">
+              <div className="text-center mb-5">
+                <h3 className="text-lime-green font-bold  text-3xl">
+                  Welcome back
+                </h3>
+                <p className="text-[#8E8E90]  text-base">
+                  Log in to your account
                 </p>
               </div>
-            </div>
-          </Form>
-        </Formik>
-      </div>
-    </section>
+
+              <div className="space-y-2 mb-6">
+                <label
+                  htmlFor="email"
+                  className="text-lime-green  font-bold text-lg"
+                >
+                  Email
+                </label>
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email."
+                  Icon={Mail}
+                />
+              </div>
+
+              <div className="space-y-2">
+                <label
+                  htmlFor="password"
+                  className="text-lime-green  font-bold text-lg"
+                >
+                  Password
+                </label>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  Icon={showPassword ? Eye : EyeOff}
+                  placeholder="Enter your password."
+                  togglePassword={togglePassword}
+                />
+              </div>
+
+              <div className="absolute bottom-0 left-0 right-0 space-y-4">
+                <div className="text-center">
+                  <AuthButton moreStyles="w-2/3 max-w-80 py-3">
+                    Log in
+                  </AuthButton>
+                </div>
+
+                <div className="text-center pb-5 text-sm space-y-2">
+                  <p className="text-gray-400">
+                    Don&apos;t have an account?
+                    <Link
+                      href="/signup"
+                      className="text-lime-green hover:underline"
+                    >
+                      &nbsp;Sign up
+                    </Link>
+                  </p>
+                  <p className="text-lime-green">
+                    &copy;copyright {new Date().getFullYear()}, All rights
+                    reserved.
+                  </p>
+                </div>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      </section>
+    </>
   );
 }
 
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -6,6 +6,7 @@ import AuthButton from "@/components/common/AuthButton";
 import logo from "@/public/icons/logo.png";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 import { FaUser } from "react-icons/fa6";
 import { Mail, Phone, Eye, EyeOff } from "lucide-react";
 import { SignUpInitialValues } from "@/interfaces";
@@ -39,73 +40,83 @@ function Signup() {
   };
 
   return (
-    <section className={`${nunito.className}`}>
-      <div className="mt-3 ml-3">
-        <Link href="/">
-          <Image
-            src={logo}
-            alt="ukulima logo"
-            className="w-9 h-9 sm:w-12 sm:h-12"
-          />
-        </Link>
-      </div>
-
-      <Formik
-        initialValues={initialValues}
-        validationSchema={signUpSchema}
-        onSubmit={handelSubmit}
-      >
-        <Form className="w-11/12 max-w-4xl mx-auto">
-          <div className="max-w-md mx-auto space-x-3 text-center mb-6">
-            <h2 className="text-lime-green font-bold text-xl md:text-3xl">
-              Create account
-            </h2>
-            <p className="text-[#8E8E90]">Register here</p>
-          </div>
-
-          <div className="grid md:grid-cols-2 md:gap-5">
-            <Input
-              type="text"
-              Icon={FaUser}
-              name="firstName"
-              placeholder="First Name"
+    <>
+      <Head>
+        <title>Sign up</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icons/favicon.ico" />
+      </Head>
+      <section className={`${nunito.className}`}>
+        <div className="mt-3 ml-3">
+          <Link href="/">
+            <Image
+              src={logo}
+              alt="ukulima logo"
+              className="w-9 h-9 sm:w-12 sm:h-12"
             />
-            <Input
-              type="text"
-              Icon={FaUser}
-              name="lastName"
-              placeholder="Last Name"
-            />
-          </div>
+          </Link>
+        </div>
 
-          <Input type="email" Icon={Mail} name="email" placeholder="Email" />
-          <Input type="text" Icon={Phone} name="phone" placeholder="Phone" />
-          <Input
-            type={showPassword ? "text" : "password"}
-            Icon={showPassword ? Eye : EyeOff}
-            name="password"
-            placeholder="Password"
-            togglePassword={togglePassword}
-          />
-
-          <div className="mt-18  text-center">
-            <AuthButton moreStyles="px-30 py-3">Sign up</AuthButton>
-
-            <div className="space-x-4 mt-5">
-              <p className="text-black/50">
-                Already have an acoount? &nbsp;
-                <Link href="/login" className="text-lime-green hover:underline">
-                  Log in
-                </Link>
-              </p>
-              <p className="text-sm text-lime-green">
-                copyright {new Date().getFullYear()}, All rights reserved.
-              </p>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={signUpSchema}
+          onSubmit={handelSubmit}
+        >
+          <Form className="w-11/12 max-w-4xl mx-auto">
+            <div className="max-w-md mx-auto space-x-3 text-center mb-6">
+              <h2 className="text-lime-green font-bold text-xl md:text-3xl">
+                Create account
+              </h2>
+              <p className="text-[#8E8E90]">Register here</p>
             </div>
-          </div>
-        </Form>
-      </Formik>
-    </section>
+
+            <div className="grid md:grid-cols-2 md:gap-5">
+              <Input
+                type="text"
+                Icon={FaUser}
+                name="firstName"
+                placeholder="First Name"
+              />
+              <Input
+                type="text"
+                Icon={FaUser}
+                name="lastName"
+                placeholder="Last Name"
+              />
+            </div>
+
+            <Input type="email" Icon={Mail} name="email" placeholder="Email" />
+            <Input type="text" Icon={Phone} name="phone" placeholder="Phone" />
+            <Input
+              type={showPassword ? "text" : "password"}
+              Icon={showPassword ? Eye : EyeOff}
+              name="password"
+              placeholder="Password"
+              togglePassword={togglePassword}
+            />
+
+            <div className="mt-18  text-center">
+              <AuthButton moreStyles="px-30 py-3">Sign up</AuthButton>
+
+              <div className="space-x-4 mt-5">
+                <p className="text-black/50">
+                  Already have an acoount? &nbsp;
+                  <Link
+                    href="/login"
+                    className="text-lime-green hover:underline"
+                  >
+                    Log in
+                  </Link>
+                </p>
+                <p className="text-sm text-lime-green">
+                  copyright {new Date().getFullYear()}, All rights reserved.
+                </p>
+              </div>
+            </div>
+          </Form>
+        </Formik>
+      </section>
+    </>
   );
 }
 
