a62fe8c60fa51a207eb12620b625f1bc3e38084f
Ephy Kimotho <ephyk48@gmail.com>
2025-03-09 21:41:38 +0300

    chore: updated base_url to deployed URL and updated next config file to allow images from that origin

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:8000"
+const BASE_URL = "https://test-ecomm.onrender.com"
 
 export { BASE_URL }
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/media/products/**',
+      },
+    ],
+    domains: ["res.cloudinary.com"],
+  },
 };
 
 export default nextConfig;
