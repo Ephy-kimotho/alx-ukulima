dad8448358276ef4b58b3345358c7325dc6fe1f6
Ephy Kimotho <ephyk48@gmail.com>
2025-03-09 21:39:10 +0300

    chore: added cart and profile links to header

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -1,6 +1,8 @@
-import { AlignRight, X } from "lucide-react";
+import { AlignRight, X, CircleUserRound } from "lucide-react";
+import { HiShoppingCart } from "react-icons/hi";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { useAuth } from "@/providers/AuthProvider";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/icons/logo.png";
@@ -9,7 +11,7 @@ import Button from "@/components/common/Button";
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { asPath } = useRouter();
-
+  const { token } = useAuth();
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -31,7 +33,7 @@ function Header() {
           href="/"
           className={`${
             asPath === "/" && "text-tangerine"
-          } font-medium text-lg  text-night hover:text-tangerine tracking-wide`}
+          } font-medium text-xl  text-night hover:text-tangerine tracking-wide`}
         >
           Home
         </Link>
@@ -39,7 +41,7 @@ function Header() {
           href="/products"
           className={`${
             asPath === "/products" && "text-tangerine"
-          }  font-medium text-lg text-night hover:text-tangerine tracking-wide`}
+          }  font-medium text-xl text-night hover:text-tangerine tracking-wide`}
         >
           Products
         </Link>
@@ -47,7 +49,7 @@ function Header() {
           href="/about"
           className={`${
             asPath === "/about" && "text-tangerine"
-          } font-medium text-lg text-night hover:text-tangerine tracking-wide`}
+          } font-medium text-xl text-night hover:text-tangerine tracking-wide`}
         >
           About us
         </Link>
@@ -55,27 +57,41 @@ function Header() {
           href="/contact"
           className={`${
             asPath === "/contact" && "text-tangerine"
-          } font-medium text-lg text-night hover:text-tangerine tracking-wide`}
+          } font-medium text-xl text-night hover:text-tangerine tracking-wide`}
         >
           Contact us
         </Link>
       </nav>
 
-      {/* Authentication buttons */}
-      <div className="hidden md:flex gap-3 items-center">
-        <Link
-          href="/login"
-          className="bg-mold-green hover:bg-white border-2 border-transparent hover:border-mold-green text-white hover:text-mold-green py-2 px-8 rounded-xl font-semibold text-lg tracking-wide"
-        >
-          Login
-        </Link>
-        <Link
-          href="/signup"
-          className="bg-white hover:bg-mold-green border-2 border-mold-green hover:border-transparent text-mold-green hover:text-white py-2 px-5 rounded-xl font-semibold text-lg tracking-wide"
-        >
-          Sign up
-        </Link>
-      </div>
+      {/* Authentication buttons and Cart */}
+      {token ? (
+        <div className="flex gap-6 ml-auto md:ml-0 items-baseline relative mr-4">
+          <Link href="/cart" className="mr-4 md:mr-0">
+            <HiShoppingCart size={35} color="#2d3134" />
+            <span className="bg-lime-green text-white text-sm font-bold p-1 px-2 rounded-full text-center absolute -top-2 left-4">
+              0
+            </span>
+          </Link>
+          <Link href="/profile" className="hidden md:block">
+            <CircleUserRound size={38} color="#2d3134" />
+          </Link>
+        </div>
+      ) : (
+        <div className="hidden md:flex gap-3 items-center">
+          <Link
+            href="/login"
+            className="bg-mold-green hover:bg-white border-2 border-transparent hover:border-mold-green text-white hover:text-mold-green py-2 px-8 rounded-xl font-semibold text-lg tracking-wide"
+          >
+            Login
+          </Link>
+          <Link
+            href="/signup"
+            className="bg-white hover:bg-mold-green border-2 border-mold-green hover:border-transparent text-mold-green hover:text-white py-2 px-5 rounded-xl font-semibold text-lg tracking-wide"
+          >
+            Sign up
+          </Link>
+        </div>
+      )}
 
       {/* Hamburger menu for mobile */}
       <Button action={toggleMenu} moreStyles="block md:hidden">
@@ -135,20 +151,30 @@ function Header() {
           </nav>
 
           {/* Authentication buttons */}
-          <div className="flex flex-col gap-2">
+          {token ? (
             <Link
-              href="/login"
-              className="bg-mold-green hover:bg-white border-2 border-transparent hover:border-mold-green text-white hover:text-mold-green py-2 px-10  rounded-xl font-semibold"
+              href="/profile"
+              className="text-night flex items-center gap-2"
             >
-              Login
+              <CircleUserRound size={30} color="#2d3134" />
+              <span> Profile</span>
             </Link>
-            <Link
-              href="/signup"
-              className="bg-white hover:bg-mold-green border-2 border-mold-green hover:border-transparent text-mold-green hover:text-white py-2 px-10 rounded-xl font-semibold"
-            >
-              Sign up
-            </Link>
-          </div>
+          ) : (
+            <div className="flex flex-col gap-2">
+              <Link
+                href="/login"
+                className="bg-mold-green hover:bg-white border-2 border-transparent hover:border-mold-green text-white hover:text-mold-green py-2 px-10  rounded-xl font-semibold"
+              >
+                Login
+              </Link>
+              <Link
+                href="/signup"
+                className="bg-white hover:bg-mold-green border-2 border-mold-green hover:border-transparent text-mold-green hover:text-white py-2 px-10 rounded-xl font-semibold"
+              >
+                Sign up
+              </Link>
+            </div>
+          )}
         </div>
       </aside>
     </header>
